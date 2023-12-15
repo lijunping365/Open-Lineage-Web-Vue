d97451ea538bae28756b6642e653b492c6a8ece3
yuxuanli <yuxuanli@digitalgd.com.cn>
2023-12-15 18:35:22 +0800

    feat: Header transform to vue

---
+++ src/components/Header/headerButton.vue
@@ -0,0 +1,63 @@
+<template>
+    <button type="button" :class="[
+        className,
+        'group rounded-md focus:outline-none focus-visible:ring-2',
+        ringClassName || (isActive ? 'focus-visible:ring-sky-500 dark:focus-visible:ring-sky-400' : 'focus-visible:ring-gray-400/70 dark:focus-visible:ring-gray-500'),
+    ].join(' ')" @click="$emit('click')">
+        <span class="sr-only">{{ label }}</span>
+        <svg :width="width" :height="height"
+            viewBox="calc((width - naturalWidth) / -2) calc((height - naturalHeight) / -2) width height" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round" :class="[
+                iconClassName,
+                isActive ? 'fill-sky-100 stroke-sky-500 dark:fill-sky-400/50 dark:stroke-sky-400' : 'fill-gray-100 stroke-gray-400/70 hover:fill-gray-200 hover:stroke-gray-400 dark:fill-gray-400/20 dark:stroke-gray-500 dark:hover:fill-gray-400/30 dark:hover:stroke-gray-400',
+            ].join(' ')">
+            <slot />
+        </svg>
+    </button>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        isActive: {
+            type: Boolean,
+            default: false,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        className: {
+            type: String,
+            default: '',
+        },
+        ringClassName: {
+            type: String,
+            default: '',
+        },
+        width: {
+            type: Number,
+            default: 42,
+        },
+        height: {
+            type: Number,
+            default: 36,
+        },
+        naturalWidth: {
+            type: Number,
+            default: 26,
+        },
+        naturalHeight: {
+            type: Number,
+            default: 22,
+        },
+        iconClassName: {
+            type: String,
+            default: '',
+        },
+    },
+});
+
+</script>
---
+++ src/components/Header/setting.vue
@@ -0,0 +1,50 @@
+<template>
+    <a-popover
+      title="设置"
+      trigger="click"
+      :open="open"
+      @openChange="(val:any) => $emit('update:open', val)"
+    >
+      <template #content>
+        <slot />
+      </template>
+      <button
+        type="button"
+        :class="[
+          'dark:shadow-highlight/4 block shadow-sm ring-1 ring-gray-900/5 hover:bg-gray-50 dark:bg-gray-800 dark:ring-0 dark:hover:bg-gray-700',
+          'group rounded-md focus:outline-none focus-visible:ring-2',
+          open
+            ? 'focus-visible:ring-sky-500 dark:focus-visible:ring-sky-400'
+            : 'focus-visible:ring-gray-400/70 dark:focus-visible:ring-gray-500',
+        ].join(' ')"
+      >
+        <span class="sr-only">设置</span>
+        <IconSetting
+          :class="[
+            open
+              ? 'fill-sky-100 stroke-sky-500 dark:fill-sky-400/50 dark:stroke-sky-400'
+              : 'fill-gray-100 stroke-gray-400/70 hover:fill-gray-200 hover:stroke-gray-400 dark:fill-gray-400/20 dark:stroke-gray-500 dark:hover:fill-gray-400/30 dark:hover:stroke-gray-400',
+          ].join(' ')"
+        />
+      </button>
+    </a-popover>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import { Popover } from 'ant-design-vue';
+import { IconSetting } from '../Icon/IconSetting';
+  
+  export default defineComponent({
+    components: {
+      Popover,
+      IconSetting,
+    },
+    props: {
+      open: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  });
+  </script>
